4158000cb523f9c92c61f747ff7683ae4a33ed71
Gunjan Shakya <gr.skya@gmail.com>
2019-09-22 21:47:18 -0500

    Brute Force Solution

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -13,3 +13,27 @@ Output: "bb"
 
 https://leetcode.com/problems/longest-palindromic-substring/
 '''
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        es = "#"
+        for chr in s: 
+            es += chr+"#"
+        p = []
+        c=0
+        while (c < len(es)):
+            l=c
+            r=c
+            if(c==0 or c == len(es)-1):
+                p.append(0)
+            else:
+                while (l>-1 and r<len(es) and es[l]==es[r]):
+                    l-=1
+                    r+=1
+                p.append(int((r-l)/2)-1)
+            c+=1
+        print(p)
+
+
+sol = Solution()
+sol.longestPalindrome("acncacn")
