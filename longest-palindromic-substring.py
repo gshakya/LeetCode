21906e6156691156bc996d964682209a8c19d6a3
Gunjan Shakya <gr.skya@gmail.com>
2019-09-30 21:08:18 -0500

    Manacer's Algorithm Solution

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -16,34 +16,48 @@ https://leetcode.com/problems/longest-palindromic-substring/
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        es = "#"
-        for chr in s: 
-            es += chr+"#"
-        p = []
-        c=0
-        maxPos = 0
-        maxVal = 0
-        while (c < len(es)):
-            l=c
-            r=c
-            if(c==0 or c == len(es)-1):
-                p.append(0)
+        if len(s) < 2:
+            return s
+        if len(s) == 2:
+            if s[0] == s[1]:
+                return s
             else:
-                while (l>-1 and r<len(es) and es[l]==es[r]):
+                return s[0]
+        
+        es ='#'+'#'.join('{}'.format(s))+'#'
+       
+        N = len(es)
+        p = [0]*N
+              
+        C=0
+        R=0
+        maxLen=0
+        mirror =0
+        pC=0
+        for i in range(0,N):
+            mirror = 2*C -i
+            if (i < R ):
+                p[i]= min(p[mirror],R-i)
+                
+            r=i + (1 + p[i])
+            l=i - (1 + p[i])
+               
+            try:
+                while (es[l]==es[r]):
                     l-=1
                     r+=1
-                if((int((r-l)/2)-1)>maxVal):
-                    maxVal = int((r-l)/2)-1
-                    p.append(maxVal)
-                    maxPos = c
-            c+=1
-
-        start = maxPos - maxVal
-        end = maxPos + maxVal
-        res = es[start:end+1]
-        
-        return(res.replace('#',''))
+                    p[i]+=1
+            except IndexError as e: 
+                pass
+            if(i+p[i]>R):
+                C,R = i,p[i]+i
+                
+            if(p[i]>maxLen):
+                pC =i    
+                maxLen = p[i]
+                
+        return(s[(pC-maxLen)//2:(pC+ maxLen)//2])
 
 
 sol = Solution()
-print(sol.longestPalindrome("acncacn"))
+print(sol.longestPalindrome("babad"))
